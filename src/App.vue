2bfee081207098155e1094b6ab8f898d565fe4c6
Yunusa Onivehu <obadobay@gmail.com>
2020-03-15 12:15:52 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Todos v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Todos from './components/Todos';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          task: "First Todo Item",
+          completed: true
+
+        },
+        {
+          id: 2,
+          task: "Second Todo Item",
+          completed: false
+
+        },
+        {
+          id: 3,
+          task: "third Todo Item",
+          completed: true
+
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+}
+body {
+  font-family:Arial, Helvetica, sans-serif;
 }
 </style>
